1aa1819deaf6ee017bbd844aa0b28f5aac75fd72
Jake Brawer <jabrawer@vassar.edu>
2015-09-23 12:00:15 -0400

    More babel syntax error fixes, this time in regards to RanksAndCrossGeneration stuff

--- RankAndCrossGeneration.py
+++ RankAndCrossGeneration.py
@@ -9,3 +9,17 @@ import datetime
 import csv
 import gc
 global_quartiles = {}
+def calculateStdError(list_of_vals, average):
+    stddev = 0.0
+    diffsquared = 0.0
+    sum_diffsquared = 0.0
+    print '\n--------------------------------------------------\nCalculating the Std Error of the mean: '
+    for val in list_of_vals:
+        diffsquared = (val- average)**2.0
+        sum_diffsquared += diffsquared 
+        print 'Org mean perf: %s Pop mean: %s Diffsqrd: %s SumDiffsqrd: %s ' % (val, average, diffsquared, sum_diffsquared)
+    stddev = ((sum_diffsquared)/len(list_of_vals))**(1.0/2.0)
+    stderror = stddev / (len(list_of_vals)**(1.0/2.0))
+    print 'Stddv: %s  StdErr: %s\n------------------------------------------------------------\n ' % (stddev, stderror)
+    #print(numpy.sqrt(numpy.var(list_of_vals)))
+    return stderror
