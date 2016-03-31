a897589c3bf01330b7ec620a97c18db165ad7a1c
Jake Brawer <jabrawer@vassar.edu>
2016-03-30 22:40:08 -0400

    ignore this

--- last_resort.py
+++ last_resort.py
@@ -8,13 +8,14 @@ def compare_offspring_to_parent(filename, offspring_gen):
         for org in f:
             org_list = (org.split(','))
             name_list = org_list[0]
-            try:
+            if not org.startswith('Filename'):
                 name_split = name_list.split('_')
                 if name_split[0] == str(offspring_gen) and name_split[1] == '1':
-                    parent1 += name_split[2]+'_'+name_split[3] 
-                    parent2 += name_split[4]+'_'+name_split[5] 
-            except IndexError:
-                pass
+                    parent1_name += name_split[2]+'_'+name_split[3] 
+                    parent2_name += name_split[4]+'_'+name_split[5] 
+    with open(filename, 'rU') as f:
         for org in f:
-            org.startswith(parent1) or org.startswith()
+            if org.startswith(parent1_name) or org.startswith(parent2_name):
+                print(org)
+
 compare_offspring_to_parent('/home/jake/org/Thesis_Stuff/Robot_Data/Development/Robot_Development_Data_Num_Threads.csv', 2)
